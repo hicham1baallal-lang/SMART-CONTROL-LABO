16e3689e2a08fb3cc07cd65f3138f84297a6a74b
hicham1baallal-lang <hicham1baallal@gmail.com>
2026-09-02 15:29:22 +0100

    Create __init__.py

---
+++ views/__init__.py
@@ -0,0 +1,6 @@
+from . import essai_Plaque
+from . import historique_pvs
+from . import suivi_Betonnage
+from . import suivi_controle_beton
+from . import synthese_Beton
+from . import synthese_plaque
