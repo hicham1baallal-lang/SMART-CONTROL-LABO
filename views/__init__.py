558e41acd8633e450377225214639d451aec1a7f
hicham1baallal-lang <hicham1baallal@gmail.com>
2026-09-02 15:30:10 +0100

    Delete views/__init__.py

--- views/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import essai_Plaque
-from . import historique_pvs
-from . import suivi_Betonnage
-from . import suivi_controle_beton
-from . import synthese_Beton
-from . import synthese_plaque
