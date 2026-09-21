1a1b14e1adb16654a066ba7247193a59dd4c0c70
hicham1baallal-lang <hicham1baallal@gmail.com>
2026-09-21 21:38:18 +0100

    Update pv_granulats.py

--- views/pv_granulats.py
+++ views/pv_granulats.py
@@ -857,7 +857,7 @@ def generate_pv_html(pv_info, info_p, data_granulats):
         font-size: 12px;
     }}
     .lpee-table th {{
-        background-color: #d9edf7;
+        background-color: #dbe5f1;
         color: #1e293b;
         border: 1px solid #808080;
         padding: 6px;
